8f22923819b94f2034ebfaf999e831b68133a8d3
Divitg1234 <72795893+Divitg1234@users.noreply.github.com>
2021-06-30 14:58:26 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 100,200)
+    fill("lightblue")
+    text("Hint: Needed to store information for programs !!", 100,220)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100,350)
+    fill("lightblue")
+    text("Hint: Used to perform programs !!", 100,370)
+
+
+// add code for displaying rest of the names and the hints.
+    
+
+}
